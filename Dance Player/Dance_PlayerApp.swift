8718dcaae3fe297549a228ecc455ead6ed28dedc
samueldesai <sdesai05@stanford.edu>
2026-06-14 18:35:14 -0700

    Initial Commit

---
+++ Dance Player/Dance_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dance_PlayerApp.swift
+//  Dance Player
+//
+//  Created by Samuel Desai on 6/14/26.
+//
+
+import SwiftUI
+
+@main
+struct Dance_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
